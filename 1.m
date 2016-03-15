b7d675a982612728564ecb1b32864007d6c46986
Floofy-KH <floofy.kh@gmail.com>
2016-03-15 14:50:14 +0000

    Initialise Repo

---
+++ 1.m
@@ -0,0 +1,3 @@
+filename = 'H:\Sim & Vim\34667__erh__lasd-suv-police-scare-kids.wav';
+[y, fs, nb] = wavread(filename);
+sound(y, fs);
---
+++ test1.m
@@ -0,0 +1,20 @@
+directory = 'H:/Sim & Vim/Sounds/';
+files = {'mid_G_guitar' 'thunder' 'police' 'scream' 'explosion' 'reverse_clang' 'woman_scream' 'water_stream'};
+
+nFiles = length(files)
+for k=1:nFiles
+file = strcat(directory, files{k});
+fprintf('Opening file: %s\n', file);
+[y, fs, nb] = wavread(file);
+L = length(y);
+ydct = dct(y);
+yfft = fft(y);
+subplot(2, nFiles, k);
+plot(y);
+title(strcat('Sound over time: ', files{k}));
+xlabel('Time');
+subplot(2, nFiles, nFiles+k);
+plot(ydct);
+title(strcat('Discrete Cosine Transform', files{k}));
+xlabel('Frequency');
+end
